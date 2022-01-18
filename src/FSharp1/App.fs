ee55d4db27ba97e2a5eb5c42ea19e631176cfcd2
Bent Rasmussen <bent.rasmussen@stati-cal.com>
2022-01-18 18:24:17 +0100

    Update to .NET 6; update to Microsoft.Xaml.Behaviors.Wpf to 1.1.39; update to FSharp.Core to 6.0.1

--- src/FSharp1/App.fs
+++ src/FSharp1/App.fs
@@ -10,5 +10,7 @@ module App =
     let main(args : string[]) =
         let app = new System.Windows.Application()
         // Relative lookup of item with BuildAction=Resource => runtime failure
+        // System.Windows.Markup.XamlParseException: ''Cannot set unknown member '{http://schemas.microsoft.com/xaml/behaviors}Interaction.Triggers'.' Line number '12' and line position '6'.'
+        // XamlObjectWriterException: 'Cannot set unknown member '{http://schemas.microsoft.com/xaml/behaviors}Interaction.Triggers'.' Line number '12' and line position '6'.
         let window = Application.LoadComponent (new Uri("Start.xaml", UriKind.Relative)) :?> Window
         app.Run(window)
